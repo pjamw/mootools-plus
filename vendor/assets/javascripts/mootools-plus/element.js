87a91f72f5f293f3f4cf2e410125b727e90ce7e6
Romain Laurent <romain.laurent@wopata.com>
2012-06-03 15:47:26 +0200

    Update getFirst, getLast and alias in Array

--- vendor/assets/javascripts/mootools-plus/element.js
+++ vendor/assets/javascripts/mootools-plus/element.js
@@ -3,20 +3,25 @@ if (window.MooTools) {
     hasElement: function(tag){
       return (tag == undefined) ? (this.getChildren().length > 0) : (this.getElement(tag) != null)
     },
+
     hasElements: function(tag){
       return (tag == undefined) ? (this.getChildren().length > 0) : (this.getElements(tag).length > 1)
     },
+
     hasEvent: function(eventType, fn){
       var myEvents = this.retrieve('events')
       return myEvents && myEvents[eventType] && (fn == undefined || myEvents[eventType].keys.contains(fn))
     },
+
     hasParent: function(tag){
       return (tag == undefined) ? (this.getParent().length > 0) : (this.getParent(tag) != null)
     },
+
     disable: function(){
       this.set('disabled', true)
       return this
     },
+
     enable: function(){
       this.set('disabled', false)
       return this
--- vendor/assets/javascripts/mootools-plus/logger.js
+++ vendor/assets/javascripts/mootools-plus/logger.js
@@ -1,4 +1,3 @@
-
 if (window.MooTools) {
   var Logger = new Class()
 
